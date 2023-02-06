c90294dc970ee28ee9c491888b535b6a29c8c41b
gonzu <gonzalodiezdavila@gmail.com>
2023-02-06 21:52:03 +0100

    Try to make it work

---
+++ src/components/listCharacters/characters.tsx
@@ -0,0 +1,33 @@
+import { charactersStructure } from "../../models/mainModels";
+export function Info({ item }: { item: charactersStructure }) {
+  return (
+    <>
+      <div className="gentleman__avatar-container">
+        <img
+          className="gentleman__avatar"
+          src={"assets/img/" + item.picture}
+          alt={item.alternativeText}
+        />
+        <span className="gentleman__initial">
+          {item.picture[0].split("")[0].toUpperCase()}
+        </span>
+      </div>
+      <div className="gentleman__data-container">
+        <h2 className="gentleman__name">{item.name}</h2>
+        <ul className="gentleman__data-list">
+          <li className="gentleman__data">
+            <span className="gentleman__data-label">Profession:</span>
+            {item.profession}
+          </li>
+          <li className="gentleman__data">
+            <span className="gentleman__data-label">Status:</span> {item.status}
+          </li>
+          <li className="gentleman__data">
+            <span className="gentleman__data-label">Twitter:</span>{" "}
+            {item.twitter}
+          </li>
+        </ul>
+      </div>
+    </>
+  );
+}
---
+++ src/models/mainModels.tsx
@@ -0,0 +1,10 @@
+export type charactersStructure = {
+  id: number;
+  name: string;
+  status: string;
+  profession: string;
+  twitter: string;
+  picture: string;
+  alternativeText: string;
+  selected: boolean;
+};
